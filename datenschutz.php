62872a31faaaf4cdf99b76aea6b2ea3524947d80
chinobi86 <a.muehlenhoff@gmx.net>
2015-01-25 13:01:46 +0100

    Datenschutz

--- datenschutz.php
+++ datenschutz.php
@@ -2,5 +2,83 @@
 	include 'config.php';
 	include 'utils.php';
 	// TODO
-	printDefault('<h1>TBD</h1>');
+	printDefault('<h1>DATENSCHUTZERKLÄRUNG</h1>
+
+<h2>Allgemeines</h2>
+
+Der Anbieter weist darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+
+Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+
+<h2>Erhebung von Zugriffsdaten</h2>
+
+Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich.
+
+Der Anbieter erhebt Daten über jeden Zugriff auf das Onlineangebot (so genannte Serverlogfiles). Zu den Zugriffsdaten gehören Name der abgerufenen Webseite, Datei, Datum und Uhrzeit des Abrufs, übertragene Datenmenge, Meldung über erfolgreichen Abruf, Browsertyp nebst Version, das Betriebssystem des Nutzers, Referrer URL (die zuvor besuchte Seite), IP-Adresse und der anfragende Provider.
+
+Der Anbieter verwendet die Protokolldaten ohne Zuordnung zur Person des Nutzers oder sonstiger Profilerstellung entsprechend den gesetzlichen Bestimmungen nur für statistische Auswertungen zum Zweck des Betriebs, der Sicherheit und der Optimierung des Onlineangebotes. Der Anbieter behält sich jedoch vor, die Protokolldaten nachträglich zu überprüfen, wenn aufgrund konkreter Anhaltspunkte der berechtigte Verdacht einer rechtswidrigen Nutzung besteht.
+
+<h2>Erhebung und Nutzung personenbezogener Daten</h2>
+
+Personenbezogene Daten werden vom Anbieter nur dann erhoben und genutzt, wenn dies gesetzlich erlaubt ist oder die Nutzer in die Datenerhebung einwilligen. In der Regel ist es für die Nutzer bei der Nutzung des Dienstes erkennbar welche Daten gespeichert werden, so zum Beispiel Name, E-Mailadresse und Nachricht bei der Nutzung des Bestellformulars.
+
+Die zum Zwecke der Bestellung von Waren angegebenen persönlichen Daten (wie zum Beispiel Name, E-Mail-Adresse, Anschrift, Zahlungsdaten) werden vom Verkäufer zur Erfüllung und Abwicklung des Vertrags verwendet. Diese Daten werden vertraulich behandelt und nicht an Dritte weitergegeben, die nicht am Bestell-, Auslieferungs- und Zahlungsvorgang beteiligt sind.
+
+Bei der Kontaktaufnahme mit dem Anbieter (per Kontaktformular oder Email) werden die Angaben des Nutzers zwecks Bearbeitung der Anfrage sowie für den Fall, dass Anschlussfragen entstehen, gespeichert.
+
+Der Anbieter hat organisatorische, vertragliche und technische Sicherheitsmaßnahmen getroffen, um sicher zu stellen, dass die Vorschriften der Datenschutzgesetze eingehalten werden und zufällige oder vorsätzliche Manipulationen, Verlust, Zerstörung oder der Zugriff unberechtigter Personen verhindert wird.
+
+<h2>Datenweiterleitung an Dritte</h2>
+
+Die Daten der Nutzer werden nur dann an Dritte weiter geleitet, wenn dies gesetzlich erlaubt ist oder ein Nutzer in die Weiterleitung eingewilligt hat.
+
+Das ist zum Beispiel der Fall wenn die Weiterleitung der Daten der Erfüllung vertraglicher Verpflichtungen gegenüber dem Nutzer dient und die Postadresse nach einer Shopbestellung an ein Speditionsunternehmen übergeben wird. Oder falls die Daten von zuständigen Stellen wie Strafverfolgungsbehörden angefordert werden.
+
+Beim Kauf von Produkten, die einen Nachweis über die Lehrtätigkeit an einer anerkannten Bildungseinrichtung erfordern, wird der vom Nutzer übermittelte Nachweis – nur soweit notwendig – an den jeweiligen Hersteller weitergeleitet, um eine Sichtprüfung des Nachweises zu ermöglichen.
+
+Die personenbezogenen Daten der Nutzer werden keineswegs an Dritte zu Werbezwecken oder zwecks Erstellung von Nutzerprofilen verkauft oder weitergeleitet.
+
+<h2>Cookies</h2>
+
+Der Anbieter setzt im Rahmen seines Onlineangebotes „Cookies“ ein. Cookies sind kleine Dateien, die auf dem Computer der Nutzer abgelegt werden und Informationen für Anbieter speichern können. Temporäre Cookies werden nach dem Schließen des Browsers gelöscht, permanente Cookies bleiben für einen vorgegebenen Zeitraum erhalten und können beim erneuten Aufruf des Onlineangebotes die gespeicherten Informationen zur Verfügung stellen.
+
+Cookies werden zum einen eingesetzt um die Nutzung des Dienstes zu erleichtern. So speichert ein Cookie den Warenkorbstatus eines Nutzers.
+
+Der Nutzer kann auf den Einsatz der Cookies Einfluss nehmen. Die meisten Browser verfügen eine Option mit der das Speichern von Cookies eingeschränkt oder komplett verhindert wird. Anbieter bemüht sich das Onlineangebot so zu gestalten, dass der Einsatz von Cookies nicht notwendig ist. Allerdings wird darauf hingewiesen, dass die Nutzung und insbesondere der Nutzungskomfort ohne Cookies eingeschränkt werden.
+
+<h2>Informationsversand per Newsletter</h2>
+
+E-Mails mit werblichen Informationen über den Anbieter sowie dessen Leistungen werden nur mit einer ausdrücklichen Einwilligung der Nutzer versendet. Die Nutzer können dem Empfang der Newsletter jederzeit widersprechen. Eine Widerspruchsmöglichkeit findet sich u.a. in jeder E-Mail.
+
+Vor dem Versand des Newsletters geht dem E-Mail-Inhaber eine Bestätigungsemail zu, in welcher er die Newsletteranmeldung bestätigen muss. Nicht bestätigte Anmeldungen werden automatisch spätestens innerhalb von vier Wochen gelöscht.
+
+Nicht zu werblichen Informationen gehören Nachrichten im Rahmen der Vertragsbeziehung mit dem Nutzer. Dazu gehört der Versand von technischen Informationen, Informationen zur Zahlungsabwicklung, Rückfragen zu Aufträgen und vergleichbare Nachrichten.
+
+Die Nutzer können nachträglich die Austragung aus der Benachrichtigungsliste per E-Mail an die oben angegebenen Kontaktmöglichkeiten beantragen.
+
+Im Rahmen der Anmeldung speichert der Anbieter den Anmelde- sowie den Bestätigungszeitpunkt und die IP-Adresse des Nutzers. Der Anbieter ist gesetzlich verpflichtet die Anmeldungen zu protokollieren, um eine ordnungsgemäße Anmeldung nachweisen zu können.
+
+<h2>Widerruf, Änderungen, Berichtigungen und Aktualisierungen</h2>
+
+Der Nutzer hat das Recht, auf Antrag unentgeltlich Auskunft zu erhalten über die personenbezogenen Daten, die vom Anbieter über ihn gespeichert wurden. Kontaktdaten finden sich im Impressum des Anbieters.
+
+Zusätzlich hat der Nutzer das Recht auf Berichtigung unrichtiger Daten, Sperrung und Löschung seiner personenbezogenen Daten, soweit keine gesetzliche Aufbewahrungspflicht entgegensteht.
+
+<h2>Datenschutzerklärung für die Nutzung von Google Analytics</h2>
+
+Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. (“Google”). Google Analytics verwendet sog. “Cookies”, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt.
+
+Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt.
+
+Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: http://tools.google.com/dlpage/gaoptout?hl=de.
+
+Sie können die Erfassung durch Google Analytics verhindern, indem Sie auf folgenden Link klicken. Es wird ein Opt-Out-Cookie gesetzt, der die zukünftige Erfassung Ihrer Daten beim Besuch dieser Website verhindert: Google Analytics deaktivieren
+
+Nähere Informationen zu Nutzungsbedingungen und Datenschutz finden Sie unterhttp://www.google.com/analytics/terms/de.html bzw. unter http://www.google.com/intl/de/analytics/privacyoverview.html. Wir weisen Sie darauf hin, dass auf dieser Website Google Analytics um den Code „ga(‘set’, ‘anonymizeIp’)“ erweitert wurde, um eine anonymisierte Erfassung von IP-Adressen (sog. IP-Masking) zu gewährleisten.
+
+Wir nutzen Google Analytics zudem dazu, Daten aus AdWords zu statistischen Zwecken auszuwerten. Sollten Sie dies nicht wünschen, können Sie dies über den Anzeigenvorgaben-Manager (http://www.google.com/settings/ads/onweb/?hl=de) deaktivieren.
+
+<h2>Änderungen der Datenschutzerklärung</h2>
+
+Der Anbieter behält sich vor, die Datenschutzerklärung zu ändern, um sie an geänderte Rechtslage oder bei Änderungen des Dienstes sowie der Datenverarbeitung anzupassen.');
 ?>
