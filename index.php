b0f18c6a4a54795cf5bf6a2b57340af341c1c079
Jens Döcke <jens.doecke@gmail.com>
2015-01-25 13:06:48 +0100

    Überschrift Style geändert und AGB Überschrift

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 	include 'config.php';
 	include 'utils.php';
-	printDefault('<h1>Sie suchen einen passenden Verein der zu Ihnen passt? <br>
-                Oder haben Sie bereits Erfahrungen mit einem Verein sammeln können <br> 
-                und wollen diese nun teilen? <br> 
-                Mit ClubHub gibt es jetzt die ideale Möglichkeit aus verschiedenen <br> 
-                Sportarten Ihren Traum Verein zu finden!<br> <br> <br> <br> </h1>');
+	printDefault('<p>Sie suchen einen passenden Verein der zu Ihnen passt?</p>
+                <p>Oder haben Sie bereits Erfahrungen mit einem Verein sammeln können
+				und wollen diese nun teilen? </p>
+                <p>Mit ClubHub gibt es jetzt die ideale Möglichkeit aus verschiedenen 
+                Sportarten Ihren Traum Verein zu finden!</p>');
 ?>
