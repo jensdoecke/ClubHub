bfe0d3da9f071ec96f9f9cf9928faae92117bd94
chinobi86 <a.muehlenhoff@gmx.net>
2015-01-25 13:07:04 +0100

    Cookies

--- datenschutz.php
+++ datenschutz.php
@@ -38,26 +38,6 @@ Beim Kauf von Produkten, die einen Nachweis über die Lehrtätigkeit an einer an
 
 Die personenbezogenen Daten der Nutzer werden keineswegs an Dritte zu Werbezwecken oder zwecks Erstellung von Nutzerprofilen verkauft oder weitergeleitet.
 
-<h2>Cookies</h2>
-
-Der Anbieter setzt im Rahmen seines Onlineangebotes „Cookies“ ein. Cookies sind kleine Dateien, die auf dem Computer der Nutzer abgelegt werden und Informationen für Anbieter speichern können. Temporäre Cookies werden nach dem Schließen des Browsers gelöscht, permanente Cookies bleiben für einen vorgegebenen Zeitraum erhalten und können beim erneuten Aufruf des Onlineangebotes die gespeicherten Informationen zur Verfügung stellen.
-
-Cookies werden zum einen eingesetzt um die Nutzung des Dienstes zu erleichtern. So speichert ein Cookie den Warenkorbstatus eines Nutzers.
-
-Der Nutzer kann auf den Einsatz der Cookies Einfluss nehmen. Die meisten Browser verfügen eine Option mit der das Speichern von Cookies eingeschränkt oder komplett verhindert wird. Anbieter bemüht sich das Onlineangebot so zu gestalten, dass der Einsatz von Cookies nicht notwendig ist. Allerdings wird darauf hingewiesen, dass die Nutzung und insbesondere der Nutzungskomfort ohne Cookies eingeschränkt werden.
-
-<h2>Informationsversand per Newsletter</h2>
-
-E-Mails mit werblichen Informationen über den Anbieter sowie dessen Leistungen werden nur mit einer ausdrücklichen Einwilligung der Nutzer versendet. Die Nutzer können dem Empfang der Newsletter jederzeit widersprechen. Eine Widerspruchsmöglichkeit findet sich u.a. in jeder E-Mail.
-
-Vor dem Versand des Newsletters geht dem E-Mail-Inhaber eine Bestätigungsemail zu, in welcher er die Newsletteranmeldung bestätigen muss. Nicht bestätigte Anmeldungen werden automatisch spätestens innerhalb von vier Wochen gelöscht.
-
-Nicht zu werblichen Informationen gehören Nachrichten im Rahmen der Vertragsbeziehung mit dem Nutzer. Dazu gehört der Versand von technischen Informationen, Informationen zur Zahlungsabwicklung, Rückfragen zu Aufträgen und vergleichbare Nachrichten.
-
-Die Nutzer können nachträglich die Austragung aus der Benachrichtigungsliste per E-Mail an die oben angegebenen Kontaktmöglichkeiten beantragen.
-
-Im Rahmen der Anmeldung speichert der Anbieter den Anmelde- sowie den Bestätigungszeitpunkt und die IP-Adresse des Nutzers. Der Anbieter ist gesetzlich verpflichtet die Anmeldungen zu protokollieren, um eine ordnungsgemäße Anmeldung nachweisen zu können.
-
 <h2>Widerruf, Änderungen, Berichtigungen und Aktualisierungen</h2>
 
 Der Nutzer hat das Recht, auf Antrag unentgeltlich Auskunft zu erhalten über die personenbezogenen Daten, die vom Anbieter über ihn gespeichert wurden. Kontaktdaten finden sich im Impressum des Anbieters.
