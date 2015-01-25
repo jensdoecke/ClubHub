e3ef023ee6ee9caea4aa9ee0c601b3b1f5ef78db
chinobi86 <a.muehlenhoff@gmx.net>
2015-01-25 17:00:13 +0100

    Impressum

--- impressum.php
+++ impressum.php
@@ -4,18 +4,26 @@
 	// TODO
 	printDefault('<h1>Verantwortlich für den Inhalt (§5 MDStV):</h1>
 
-<p>Club Hub GmbH
-Clubstrasse. 38a
-85561 Clubhausen 
-Deutschland</p>
+<p>Club Hub GmbH</p>
+<p>Clubstrasse. 38a</p>
+<p>85561 Clubhausen </p>
+<p>Deutschland</p>
 
 <p>vertreten durch:</p>
 
 <p>Geschäftsführer: Hermann Club</p>
 
+<<<<<<< Updated upstream
 <p>Telefonnummer: +49 (0)89 123-0
 Faxnummer: +49 (0)89 123-30
 E-Mail Adresse: info@clubhub.de
 Registergericht & Registernummer: Amtsgericht München: HRB 123456
 Umsatzsteuer-Identifikationsnummer: DE 123 456 789</p>');
+=======
+<p>Telefonnummer: +49 (0)89 123-0</p>
+<p>Faxnummer: +49 (0)89 123-30</p>
+<p>E-Mail Adresse: info@clubhub.de</p>
+<p>Registergericht & Registernummer: Amtsgericht München: HRB 123456</p>
+<p>Umsatzsteuer-Identifikationsnummer: DE 123 456 789</p>’);
+>>>>>>> Stashed changes
 ?>
